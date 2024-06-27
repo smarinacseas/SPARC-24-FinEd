c2d330646b59cb963fe43adec8a9773ea7e40a9f
smarinacseas <smarinac@seas.upenn.edu>
2024-06-27 13:36:11 -0500

    Basic app.py implemented

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+# Load env vars
+load_dotenv()
+
+app = Flask(__name__)
+
+# Access env vars
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug = True)
