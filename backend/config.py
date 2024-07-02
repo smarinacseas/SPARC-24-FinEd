e6f8d7659d413f931fe380a606249e5f71d1c298
jyswuu <105914684+jyswuu@users.noreply.github.com>
2024-07-01 21:54:29 -0700

    added to req.txt and created config file

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://postgres:Welcome1@financial-app-db.c92cqe6w64ar.us-east-2.rds.amazonaws.com:5432/initial_fin_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
