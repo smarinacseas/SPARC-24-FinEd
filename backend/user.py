9bfa13fff0ed6ae9e1d9234795cf80f53290261a
jyswuu <105914684+jyswuu@users.noreply.github.com>
2024-07-02 00:58:28 -0700

    edit database info and added sample register file html to test creating new login

--- backend/user.py
+++ backend/user.py
@@ -3,9 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    __tablename__ = 'customer_db'
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    occupation= db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return f'<User {self.username}>'
