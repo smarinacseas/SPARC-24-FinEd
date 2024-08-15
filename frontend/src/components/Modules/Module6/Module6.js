8e4ef021857ac67f3fa3dc43ffe6d240b3b91566
jiyiw <167023624+jiyiw@users.noreply.github.com>
2024-08-14 19:27:12 -0700

    updated module 6

--- frontend/src/components/Modules/Module6/Module6.js
+++ frontend/src/components/Modules/Module6/Module6.js
@@ -19,24 +19,24 @@ function Module6() {
   const handleCheckboxChange = async (item) => {
     setCheckedItems((prev) => {
       const updatedCheckedItems = { ...prev, [item]: !prev[item] };
-      updateProgress(updatedCheckedItems); // Update progress on checkbox change
+      //updateProgress(updatedCheckedItems); // Update progress on checkbox change
       return updatedCheckedItems;
     });
   };
 
   // Function to update progress in the backend
-  const updateProgress = async (updatedCheckedItems) => {
-    const totalActions = Object.keys(updatedCheckedItems).length;
-    const completedActions = Object.values(updatedCheckedItems).filter(Boolean).length;
-    const progress = (completedActions / totalActions) * 100;
+  //const updateProgress = async (updatedCheckedItems) => {
+  //  const totalActions = Object.keys(updatedCheckedItems).length;
+  //  const completedActions = Object.values(updatedCheckedItems).filter(Boolean).length;
+  //  const progress = (completedActions / totalActions) * 100;
 
-    try {
+  //  try {
       // Send progress to backend
-      await axios.post('/api/update-progress', { module: 'module5', progress });
-    } catch (error) {
-      console.error('Error updating progress:', error);
-    }
-  };
+  //    await axios.post('/api/update-progress', { module: 'module5', progress });
+  //  } catch (error) {
+  //    console.error('Error updating progress:', error);
+  //  }
+  //};
 
 
   // Function to handle input changes
